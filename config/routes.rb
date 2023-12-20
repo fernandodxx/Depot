004e5fda6b750ee37c3b33292901bd0ca7bfd27d
fernandoxx <fernandosillvasantoss@gmail.com>
2023-12-20 18:31:52 -0300

    Add traduçao en - es

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,25 @@
 Rails.application.routes.draw do
-  get 'admin/index'
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  get 'sessions/create'
+  get 'sessions/destroy'
+
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
-  
-  resources :products do 
+  resources :products do
     get :who_bought, on: :member
   end
 
-  get 'admin' => 'admin#index'
-    controller :sessions do
-      get 'login' => :new
-      post 'login' => :create
-      delete 'logout' => :destroy
-    end
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
